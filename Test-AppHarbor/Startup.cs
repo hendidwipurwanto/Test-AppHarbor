3c00d2125d4b1936d6745959a125bf480cc4eb47
hendi.dwipurwanto <hendi.dwipurwanto@ogilvy.com.au>
2016-07-20 14:18:24 +0800

    add project

---
+++ Test-AppHarbor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test_AppHarbor.Startup))]
+namespace Test_AppHarbor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
